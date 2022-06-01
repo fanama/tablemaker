ccc57d6cd3164e730d19feb2eea2278eaf66d2ac
fanama <fana.rakotoasimbola@clirisgroup.com>
2022-06-01 21:21:29 +0200

    pnpm

--- index.tsx
+++ index.tsx
@@ -1,33 +1,33 @@
-import * as React from 'react'
+import React from "react";
 
 export function TableMaker({ attributes }: any) {
-    // eslint-disable-next-line react/prop-types
-    const lines = attributes.map((attr: any, index: number) => {
-        const headers = Object.keys(attr)
-
-        return (
-            <tr key={index}>
-                {headers.map((header: string, index: number) => {
-                    return <td key={index}>{attr[header]}</td>
-                })}
-            </tr>
-        )
-    })
-
-    const headers = attributes[0] ? Object.keys(attributes[0]) : []
+  // eslint-disable-next-line react/prop-types
+  const lines = attributes.map((attr: any, index: number) => {
+    const headers = Object.keys(attr);
 
     return (
-        <div className="tableMaker">
-            <table>
-                <thead>
-                    <tr>
-                        {headers.map((header: string, index: number) => {
-                            return <th key={index}>{header}</th>
-                        })}
-                    </tr>
-                </thead>
-                <tbody>{lines}</tbody>
-            </table>
-        </div>
-    )
+      <tr key={index}>
+        {headers.map((header: string, index: number) => {
+          return <td key={index}>{attr[header]}</td>;
+        })}
+      </tr>
+    );
+  });
+
+  const headers = attributes[0] ? Object.keys(attributes[0]) : [];
+
+  return (
+    <div className="tableMaker">
+      <table>
+        <thead>
+          <tr>
+            {headers.map((header: string, index: number) => {
+              return <th key={index}>{header}</th>;
+            })}
+          </tr>
+        </thead>
+        <tbody>{lines}</tbody>
+      </table>
+    </div>
+  );
 }
